96baa1c5a6dcba31137f79b966a4f34f0aba707a
Hongquan Li <hqlisu@gmail.com>
2020-02-19 04:37:00 -0500

    add magnification selection

--- LDMTileScanSetupGUI.py
+++ LDMTileScanSetupGUI.py
@@ -12,16 +12,16 @@ import math
 import xml.etree.ElementTree as ET
 import MAFtool
 
-DEBUG = True
+DEBUG = False
 
 # DESKTOP_DIR = 'C:\\Users\\Wang Lab\\Desktop'
 DESKTOP_DIR = os.path.expanduser("~/Desktop")
 
 # microscope defination
-M = 10
-FOV =(11.6*1e-3/M) # in meter
+FOV = 11.6*1e-3 # in meter
 OVERLAP = 0.15
 OFFSET = 0 # offset of the center of the FOV from the coordinate read from the RGN file
+MAGNIFICATIONS = [10,25,40]
 
 class tileScanSetupGUI(QMainWindow):
 
@@ -43,6 +43,9 @@ class tileScanSetupGUI(QMainWindow):
         self.entry_ZPosition.setFrameStyle(QFrame.Panel | QFrame.Sunken)
         self.button_loadAFCOffsetandZPosition = QPushButton("Load from file")
         self.button_loadAFCOffsetandZPosition.clicked.connect(self.loadAFCOffsetandZPositionFromFile)
+
+        self.dropdown_Magnification = QComboBox()
+        self.dropdown_Magnification.addItems([str(M)+'x' for M in MAGNIFICATIONS])
         
         self.regionlist = QListWidget()
 
@@ -60,12 +63,9 @@ class tileScanSetupGUI(QMainWindow):
 
         # arrange entry boxes and buttons
         hbox = QHBoxLayout()
-        hbox.addWidget(QLabel('AFC Offset'))
-        hbox.addWidget(self.entry_AFCOffset)
-        hbox.addWidget(QLabel('Z Position'))
-        hbox.addWidget(self.entry_ZPosition)
-        hbox.addWidget(self.button_loadAFCOffsetandZPosition)
-
+        hbox.addWidget(QLabel('Objective Magnification'))
+        hbox.addWidget(self.dropdown_Magnification)
+        
         frame_1 = QFrame()
         frame_1.setLayout(hbox)
         frame_1.setFrameStyle(QFrame.Panel | QFrame.Raised)
@@ -81,19 +81,32 @@ class tileScanSetupGUI(QMainWindow):
         frame_2.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Minimum)
 
         hbox = QHBoxLayout()
-        hbox.addWidget(self.lineEdit_MAFFileName)
-        hbox.addWidget(self.button_generateLDMMAFFile)
+        hbox.addWidget(QLabel('AFC Offset'))
+        hbox.addWidget(self.entry_AFCOffset)
+        hbox.addWidget(QLabel('Z Position'))
+        hbox.addWidget(self.entry_ZPosition)
+        hbox.addWidget(self.button_loadAFCOffsetandZPosition)
 
         frame_3 = QFrame()
         frame_3.setLayout(hbox)
         frame_3.setFrameStyle(QFrame.Panel | QFrame.Raised)
         frame_3.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Minimum)
 
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.lineEdit_MAFFileName)
+        hbox.addWidget(self.button_generateLDMMAFFile)
+
+        frame_4 = QFrame()
+        frame_4.setLayout(hbox)
+        frame_4.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        frame_4.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Minimum)
+
         # set up the central widget
         layout = QGridLayout()
         layout.addWidget(frame_1,0,0)
         layout.addWidget(frame_2,1,0)
         layout.addWidget(frame_3,2,0)
+        layout.addWidget(frame_4,2,0)
         self.centralWidget = QWidget()
         self.centralWidget.setLayout(layout)
         self.setCentralWidget(self.centralWidget)
@@ -111,6 +124,7 @@ class tileScanSetupGUI(QMainWindow):
             self.loaded_MAFfile = MAFfile
 
     def addTileRegion(self):
+        
         dialog = QFileDialog()
         RGNfile, _filter = dialog.getOpenFileName(None,'Open File',DESKTOP_DIR,'XML files (*.rgn)')
         if RGNfile != '':
@@ -122,6 +136,9 @@ class tileScanSetupGUI(QMainWindow):
             shapeList = regions.find('ShapeList')
             regions = shapeList.find('Items')
 
+            # update FOV
+            magnification = int(self.dropdown_Magnification.currentText()[:-1])
+            FOV_object = FOV/magnification
             # iterate through tile regions and populate the MAFtree
             for region in regions:
                 region_type = region.find('Type')
@@ -136,7 +153,7 @@ class tileScanSetupGUI(QMainWindow):
                     point_top_right = items.find('Item3')
                     x1 = float(point_top_right.find('X').text)
                     y1 = float(point_top_right.find('Y').text)
-                    d = FOV*(1-OVERLAP)
+                    d = FOV_object*(1-OVERLAP)
                     nx = math.ceil((x1-x0)/d)
                     ny = math.ceil((y1-y0)/d)
                     if DEBUG:
